667ce9a33df2a6fcc81f9e3f6c29aa8b2fa325c5
JAHANZAIB <zain070123@gmail.com>
2024-09-15 11:24:02 -0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import {jsPDF} from 'jspdf'
 import { read } from "fs";
+import next from "next/types";
 
 
 
@@ -14,6 +15,7 @@ export default function MultiStepForm() {
     lastName: "",
     city: "",
     email: "",
+    phoneNumber:"",
     image: "",
     selectQualifi: "",
     schoolName: "",
@@ -79,64 +81,60 @@ export default function MultiStepForm() {
     const marginX = 10;
     let currentY = 10;
   
+// Add Name and Contact information 
+doc.setFontSize(18);
+doc.text(`${formData.firstName}${formData.lastName}`,marginX,currentY);
+currentY += 10;
+doc.setFontSize(12);
+doc.text(`${formData.city}|${formData.email}|${formData.phoneNumber}`,marginX, currentY);
+currentY += 20;
 
 
+doc.setFontSize(14);
+doc.text("Education",marginX,currentY);
+currentY += 10;
+doc.setFontSize(12);
+doc.text(`Degree:${formData.selectDegree}`, marginX, currentY);
+currentY += 10;
+doc.text(`institute:${formData.schoolName}`,marginX, currentY);
+currentY += 20;
 
-    if (formData.image) {
-      doc.addImage(formData.image, 'JPEG', marginX, currentY, 50, 50); // X, Y, Width, Height
-      currentY += 60; // Add space after the image
-    }
-    // Add Name and Contact Information
-    doc.setFontSize(18);
-    doc.text(`${formData.firstName} ${formData.lastName}`, marginX, currentY);
-    currentY += 10;
-    doc.setFontSize(12);
-    doc.text(`${formData.city} | ${formData.email}`, marginX, currentY);
-    currentY += 20;
 
-   
-    doc.setFontSize(14);
-    doc.text("Education", marginX, currentY);
-    currentY += 10;
-    doc.setFontSize(12);
-    doc.text(`Degree: ${formData.selectDegree}`, marginX, currentY);
-    currentY += 10;
-    doc.text(`Institute: ${formData.schoolName}`, marginX, currentY);
-    currentY += 20;
-  
-   
-    doc.setFontSize(14);
-    doc.text("Experience", marginX, currentY);
-    currentY += 10;
-    doc.setFontSize(12);
-    doc.text(`Job Title: ${formData.jobTitle}`, marginX, currentY);
-    currentY += 10;
-    doc.text(`Employer: ${formData.employer}`, marginX, currentY);
-    currentY += 10;
-    doc.text(`Location: ${formData.jobLocation}`, marginX, currentY);
-    currentY += 10;
-    doc.text(`Duration: ${formData.startDate} to ${formData.endDate} ${formData.isCurrentlyWorking ? "(Currently Working)" : ""}`, marginX, currentY);
-    currentY += 20;
-  
-    doc.setFontSize(14);
-    doc.text("Skills", marginX, currentY);
-    currentY += 10;
-    doc.setFontSize(12);
-    doc.text(`${formData.skills}`, marginX, currentY);
-    currentY += 20;
-  
-    doc.setFontSize(14);
-    doc.text("Bio", marginX, currentY);
-    currentY += 10;
-    doc.setFontSize(12);
-    doc.text("I am a motivated and detail-oriented professional with a solid background in my field.", marginX, currentY);
-    currentY += 10;
-    doc.text("With a strong passion for continuous growth and development, I bring valuable skills to any role.", marginX, currentY);
-    currentY += 10;
-    doc.text("My focus is on leveraging my expertise to contribute effectively to team goals and drive positive results.", marginX, currentY);
-  
-    doc.save("cv.pdf");
-  }
+doc.setFontSize(14);
+doc.text("experience",marginX, currentY);;
+currentY += 10;
+doc.setFontSize(12);
+doc.text(`Job Title : ${formData.jobTitle}`, marginX , currentY);
+currentY += 10;
+doc.text(`Employer: ${formData.employer}`, marginX, currentY);
+currentY += 10;
+doc.text(`Location: ${formData.jobLocation}`, marginX , currentY);
+currentY += 10;
+doc.text(`Duration:${formData.startDate} to ${formData.endDate} ${formData.isCurrentlyWorking ?"(Currently Working)": ""}`, marginX, currentY);
+currentY += 20;
+
+
+doc.setFontSize(14);
+doc.text("Skills", marginX, currentY);
+currentY += 10;
+doc.setFontSize(12);
+doc.text(`${formData.skills}`, marginX , currentY);
+currentY += 20;
+
+
+doc.setFontSize(14);
+doc.text("Bio" , marginX, currentY);
+currentY += 20;
+doc.setFontSize(12);
+doc.text("I am a motivated and detail-oriented professional with a solid background in my field.", marginX , currentY);
+currentY += 10;
+doc.text("With a strong passion for continuous growth and development, I bring valuable skills to any role.", marginX, currentY);
+currentY += 10;
+doc.text("My focus is on leveraging my expertise to contribute effectively to team goals and drive positive results.",marginX , currentY);
+doc.save("cv.pdf");
+
+   }
+    
 
 
   
@@ -165,6 +163,10 @@ export default function MultiStepForm() {
           <h4>Enter Your Email</h4>
           <input type="email" id="email" value={formData.email} onChange={handleInputChange} required placeholder="e.g JohnBerk@gmail.com"/>
           <br />
+          <br />
+          <h4>Enter Your Contact Number</h4>
+          <input type="number" id="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange}  required placeholder="e.g +923323847854"/>
+          <br />
           <div className="file-upload-container">
           <h4>Upload Your Profile Picture here.</h4>
           <input type="file" accept="image/*" onChange={handleFileChange} />
@@ -190,141 +192,83 @@ export default function MultiStepForm() {
         </select>
         <br />
         <br />
-        <h4>Institute Name</h4>
-        <input id="schoolName" type="text" value={formData.schoolName} onChange={handleInputChange} placeholder="e.g {School or College Name}" />
-        <br />
-        <br />
-        <h4>Your Degree</h4>
-        <select id="selectDegree" value={formData.selectDegree} onChange={handleInputChange}>
-          <option value="High School Diploma">High School Diploma</option>
-          <option value="GED">GED</option>
-          <option value="Associates of Arts">Associates of Arts</option>
-          <option value="Associates of Science">Associates of Science</option>
-          <option value="Masters of Arts">Masters of Arts</option>
-          <option value="Masters of Science">Masters of Science</option>
-          <option value="Ph.D.">Ph.D.</option>
-          <option value="MBA">MBA</option>
-          <option value="J.D">J.D</option>
-          <option value="M.D">M.D</option>
-          <option value="NO DEGREE">NO DEGREE</option>
-        </select>
-        <br />
-        <br />
-        <button type="button" onClick={prevStep}>Previous</button>
-        <button type="button" onClick={nextStep}>Next</button>
-        <br />
-      </form>
+       <h4>Institute Name</h4>
+       <input id="schoolName" type="text" value={formData.schoolName} onChange={handleInputChange} placeholder="e.g {School or College Name}" />
+       <br />
+       <br />
+       <h4>Your Degree</h4>
+       <select name="" id="selectDegree" value={formData.selectDegree} onChange={handleInputChange}>
+       <option value="High School Diploma">High School Diploma</option>
+       <option value="GED">GED</option>
+       <option value="Associates of Arts">Associates of Arts</option>
+       <option value="Associates of Science">Associates of Science</option>
+       <option value="Masters of Arts">Masters of Arts</option>
+       <option value="Masters of Science">Masters of Science</option>
+       <option value="Ph.D">Ph.D</option>
+       <option value="MBA">MBA</option>
+       <option value="J.D">J.D</option>
+       <option value="M.D">M.D</option>
+       <option value="NO DEGREE">NO DEGREE</option>
+       </select>
+       <br />
+       <br />
+       <button type="button" onClick={prevStep}>Previous</button>
+       <button type="button" onClick={nextStep}></button>
+       <br />
+       </form>
     )}
+
+
+
 
     {/* Form 3 = Experience */}
-    {step === 3 && (
-      <form className="center-text">
-        <h1>Tell us about your most recent job</h1>
-        <h4>Job Title</h4>
-        <input id="jobTitle" type="text" value={formData.jobTitle} onChange={handleInputChange} required placeholder="e.g Sale Associate " />
-        <h4>Employer</h4>   
-        <input id="employer" type="text" value={formData.employer} onChange={handleInputChange} required placeholder="e.g ACME Technologies" />
-        <h4>Location</h4>
-        <input id="jobLocation" type="text" value={formData.jobLocation} onChange={handleInputChange} placeholder="e.g Lahore, Punjab" />
-        <h4>Start Date</h4>
-        <input type="date" id="startDate" value={formData.startDate} onChange={handleInputChange} required/>
-        <h4>End Date</h4>
-        <input type="date" id="endDate" value={formData.endDate} onChange={handleInputChange} />
-        <br />
-        <br />
-        <input type="checkbox" id="isCurrentlyWorking" checked={formData.isCurrentlyWorking} onChange={handleInputChange} />
-        <label htmlFor="isCurrentlyWorking"> I currently work here</label>
-        <br />
-        <br />
-        <button type="button" onClick={prevStep}>Previous</button>
-        <button type="button" onClick={nextStep}>Next</button>
-      </form>
-    )}
+  {step === 5 && (
+    <div className="center-text">
+      <h1>Curriculum Vitae</h1>
+      <div className="cv-header">
+        {formData.image && (
+          <div>
+            <h4>Profile Picture:</h4>
+            <img src={formData.image} alt="Profile Picture" style={{width: "110px", borderRadius:"50%"}} />
 
-    {/* Form 4 = Skills */}
-    {step === 4 && (
-      <form className="center-text">
-        <h1>Skills</h1>
-        <label htmlFor="skills">    Select your skills    </label>
-        <select id="skills" value={formData.skills} onChange={handleInputChange} required>
-          <option value="JavaScript">JavaScript</option>
-          <option value="Python">Python</option>
-          <option value="HTML">HTML</option>
-          <option value="CSS">CSS</option>
-          <option value="React">React</option>
-          <option value="Node.js">Node.js</option>
-          <option value="UI/UX Design">UI/UX Design</option>
-          <option value="Cybersecurity">Cybersecurity</option>
-          <option value="Customer Service">Customer Service</option>
-          <option value="Sales Associate">Sales Associate</option>
-          <option value="Administrative Assistant">Administrative Assistant</option>
-          <option value="Data Entry">Data Entry</option>
-          <option value="Sales Management">Sales Management</option>
-          <option value="Market Research">Market Research</option>
-          <option value="Lead Generation">Lead Generation</option>
-          <option value="Sales Forecasting">Sales Forecasting</option>
-          <option value="Customer Relationship Management (CRM)">Customer Relationship Management (CRM)</option>
-          <option value="Sales Strategy">Sales Strategy</option>
-          <option value="Product Knowledge">Product Knowledge</option>
-        </select>
-        <br />
-        <br />
-        <button type="button" onClick={prevStep}>Previous</button>
-        <button type="button" onClick={handleSubmit}>Submit</button>
-      </form>
-    )}
+          </div>
+        )}
+        <div>
+          <h2>{formData.firstName}|{formData.lastName}</h2>
+          <p>{formData.city}|{formData.email}|{formData.phoneNumber}</p>
 
-  
-{/* CV Output */}
-{step === 5 && (
-  <div className="center-text">
-    <h1>Curriculum Vitae</h1>
-    <div className="cv-header">
-    {formData.image && (
-      <div>
-        <h4>Profile Picture:</h4>
-        <img src={formData.image} alt="Profile Picture" style={{ width: "110px", borderRadius: "50%" }} />
+        </div>
       </div>
-    )}
-      <div>
-        <h2>{formData.firstName} {formData.lastName}</h2>
-        <p>{formData.city} | {formData.email}</p>
-      </div>
+
+      <section>
+        <h3>Education</h3>
+        <p><strong>Degree:</strong> {formData.selectDegree}</p>
+        <p><strong>Institute:</strong> {formData.schoolName}</p>
+      </section>
+      <section>
+        <h3>Experience</h3>
+        <p><strong>Job Title:</strong> {formData.selectDegree}</p>
+        <p><strong>Employer:</strong> {formData.employer}</p>
+        <p><strong>Location:</strong> {formData.jobLocation}</p>
+        <p><strong>Duration:</strong> {formData.startDate} to {formData.endDate}{formData.isCurrentlyWorking ? "(Currently Working)": ""}</p>   
+      </section>
+      <section>
+        <h3>Skills</h3>
+        <p>{formData.skills}</p>
+      </section>
+      <section>
+        <h3>Bio</h3>
+        <p>
+        I am a motivated and detail-oriented professional with a solid background in my field. With a strong passion for continuous growth and development, I bring valuable skills and experience to any role. My focus is on leveraging my expertise to contribute effectively to team goals and drive positive results.
+        </p>
+      </section>
+      <br />
+      <br />
+
+      <button onClick={prevStep}>Edit Details</button>
+      <button onClick={handleDownload}>Download CV</button>
     </div>
-    <section>
-      <h3>Education</h3>
-      <p><strong>Degree:</strong> {formData.selectDegree}</p>
-      <p><strong>Institute:</strong> {formData.schoolName}</p>
-    </section>
-    <section>
-      <h3>Experience</h3>
-      <p><strong>Job Title:</strong> {formData.jobTitle}</p>
-      <p><strong>Employer:</strong> {formData.employer}</p>
-      <p><strong>Location:</strong> {formData.jobLocation}</p>
-      <p><strong>Duration:</strong> {formData.startDate} to {formData.endDate} {formData.isCurrentlyWorking ? "(Currently Working)" : ""}</p>
-    </section>
-    <section>
-      <h3>Skills</h3>
-      <p>{formData.skills}</p>
-    </section>
-    <section>
-    <h3>Bio</h3>
-  <p>
-    I am a motivated and detail-oriented professional with a solid background in my field. With a strong passion for continuous growth and development, I bring valuable skills and experience to any role. My focus is on leveraging my expertise to contribute effectively to team goals and drive positive results.
-  </p>
-  <p>
-    Throughout my career, I have developed a comprehensive understanding of industry practices and honed my skills through various projects. I am committed to staying updated with industry trends and dedicated to making a meaningful impact in dynamic environments.
-  </p>
-     </section>
-  <br />
-  <br />
-
-  <button onClick={prevStep}>Edit Details</button>
-  <button onClick={handleDownload}>Download CV</button>
-  
-  </div>
-  
-)}
+  )}
 
   </>
   );
@@ -335,3 +279,4 @@ export default function MultiStepForm() {
 
 
 
+// tsx = typescript exenel 
